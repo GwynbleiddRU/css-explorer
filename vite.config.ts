1993b12501c686ceddb25c5e3b81c30f407c4555
George Nosachev <nosachev.gosha@gmail.com>
2025-04-11 01:00:40 +0500

    feat: basename  and homepage for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/css-explorer',
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
   },
   plugins: [
     react(),
